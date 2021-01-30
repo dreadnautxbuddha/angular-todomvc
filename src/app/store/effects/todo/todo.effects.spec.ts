e900eba18447c6b4e8b7ac1e802f0c1a5b18af5a
Peter Cortez <innov.petercortez@gmail.com>
2021-01-30 15:19:39 +0800

    Delete todo item when its description is cleared

--- src/app/store/effects/todo/todo.effects.spec.ts
+++ src/app/store/effects/todo/todo.effects.spec.ts
@@ -1,13 +1,14 @@
-import { Action } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
+import { empty, Observable, of } from 'rxjs';
+import { Action, select } from '@ngrx/store';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { TodoEffects } from './todo.effects';
 import { AppState } from '../../models/app-state';
-import { CreateTodoAction } from '../../actions/todo.actions';
+import { allTodos } from '../../selectors/todo/todo.selector';
 import { UpdateInputAction } from '../../actions/input.action';
+import { CreateTodoAction, UpdateTodoAction } from '../../actions/todo.actions';
 import { initialState as initialTodoState } from '../../../store/reducers/todo/todo.reducer';
 import { initialState as initialInputState } from '../../../store/reducers/input/input.reducer';
 
@@ -44,11 +45,41 @@ describe('TODO Effects', () => {
       }));
 
       effects.clearInputOnTodoCreate$.subscribe(state => {
-        console.log(state)
         expect(state).toEqual({ description: null, type: '[INPUT] Update' });
 
         done();
       });
+    });
+  });
+
+  describe('On UpdateTodoAction', () => {
+    describe('when todo\'s description is cleared ', () => {
+      let emptyDescriptions = [
+        '',
+        null,
+      ];
+
+      emptyDescriptions.forEach(emptyDescription => {
+        it(`should be deleted when description is ${emptyDescription}`, (done) => {
+          let todo = {
+            id: 'b50e735f-d38a-4fe8-96f7-446ba8883342',
+            description: 'A todo item',
+            isCompleted: false,
+            isEditing: false,
+          };
+          let emptyTodo = { ...todo, description: emptyDescription };
+
+          // Lookie here! The user has cleared the todo item's description. Bad
+          // cheetah!!
+          actions$ = of(UpdateTodoAction(emptyTodo));
+
+          effects.deleteTodoWhenDescriptionIsEmpty$.subscribe(state => {
+            expect(state).toEqual({ ...emptyTodo, type: '[TODO] Delete' });
+
+            done();
+          });
+        });
+      })
     });
   });
 });
--- src/app/store/effects/todo/todo.effects.ts
+++ src/app/store/effects/todo/todo.effects.ts
@@ -1,10 +1,10 @@
-import { of } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { CreateTodoAction } from '../../actions/todo.actions';
 import { UpdateInputAction } from '../../actions/input.action';
+import { CreateTodoAction, DeleteTodoAction, UpdateTodoAction } from '../../actions/todo.actions';
 
 @Injectable()
 export class TodoEffects {
@@ -18,6 +18,17 @@ export class TodoEffects {
     () => this.actions$.pipe(
       ofType(CreateTodoAction),
       switchMap(action => of(UpdateInputAction({ description: null }))),
+    )
+  );
+
+  deleteTodoWhenDescriptionIsEmpty$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(UpdateTodoAction),
+      switchMap(
+        action => (action.description || '').trim().length === 0
+          ? of(DeleteTodoAction(action))
+          : EMPTY
+      )
     )
   );
 
